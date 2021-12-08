35769b7d1c0676746584558c652527e887e257c1
Agung Priambada <thezeneight@gmail.com>
2021-12-08 20:04:41 +0800

    add command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         //
+        Commands\CekWeb::class,
     ];
 
     /**
@@ -25,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('custom:cekweb')->everyMinute();
     }
 
     /**
